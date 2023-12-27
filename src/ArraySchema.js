6038a95a1c251f637f15edc4332f07cf645bd8bd
Анастасия Мамаева <hdekjsne@gmail.com>
2023-12-27 14:38:05 +0300

    third step

--- src/ArraySchema.js
+++ src/ArraySchema.js
@@ -1,6 +1,7 @@
 export default class ArraySchema {
 	constructor() {
 		this.intsOnly = false;
+		this.customValidator = 'none';
 	}
 
 	allIntegers() {
@@ -8,13 +9,36 @@ export default class ArraySchema {
 		return this;
 	}
 
+	custom(func) {
+		this.customValidator = func;
+		return this;
+	}
+
 	isValid(value) {
+		let isValid = true;
+		if (!Array.isArray(value)) isValid = false;
+		if (this.intsOnly && isValid) isValid = !value
+			.map((item) => Number.isInteger(item))
+			.includes(false);
+		if (this.customValidator !== 'none' && isValid) isValid = !value
+			.map((item) => this.customValidator(item))
+			.includes(false);
+		return isValid;
+		/*
 		if (!Array.isArray(value)) return false;
-		if (this.intsOnly) {
-			return	!value
-				.map((item) => typeof item === 'number' && !item.toString().includes('.'))
-				.includes(false);
-		}
-		return true;
+		const validations = [
+			...(
+				this.allIntegers === true
+					? value.map((item) => typeof item === 'number')
+					: [true]
+			),
+			(
+				this.customValidator !== 'none'
+					?	value.map((item) => this.customValidator(item))
+					: [true]
+			)
+		];
+		return !validations.includes(false);
+		*/
 	}
 }
